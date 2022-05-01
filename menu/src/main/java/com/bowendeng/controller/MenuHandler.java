291292229ac0312a4fe92fb6749af0abdf17557e
Bowen Deng <bd45@rice.edu>
2022-05-01 15:05:23 -0500

    allow service consumer(eureka client) to call service(eureka server)

--- menu/src/main/java/com/bowendeng/controller/MenuHandler.java
+++ menu/src/main/java/com/bowendeng/controller/MenuHandler.java
@@ -1,6 +1,7 @@
 package com.bowendeng.controller;
 
 import com.bowendeng.entity.Menu;
+import com.bowendeng.entity.MenuVO;
 import com.bowendeng.repository.MenuRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -26,7 +27,8 @@ public class  MenuHandler {
     private MenuRepository menuRepository;
 
     @GetMapping("/findAll/{index}/{limit}")
-    public List<Menu> findAll(@PathVariable("index") int index, @PathVariable("limit") int limit){
-        return menuRepository.findAll(index, limit);
+    public MenuVO findAll(@PathVariable("index") int index, @PathVariable("limit") int limit){
+        List<Menu> list = menuRepository.findAll(index, limit);
+        return new MenuVO(0, "", 100, list);
     }
 }
