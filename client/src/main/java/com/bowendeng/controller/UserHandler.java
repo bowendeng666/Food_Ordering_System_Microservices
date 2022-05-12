5c85d89476bad964c5b877e3039dc884ba292400
Bowen Deng <bd45@rice.edu>
2022-05-12 01:15:05 -0500

    user add, delete

--- client/src/main/java/com/bowendeng/controller/UserHandler.java
+++ client/src/main/java/com/bowendeng/controller/UserHandler.java
@@ -1,28 +1,39 @@
 package com.bowendeng.controller;
 
 import com.bowendeng.entity.User;
+import com.bowendeng.entity.UserVO;
 import com.bowendeng.feign.UserFeign;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping("/user")
 public class UserHandler {
 
     @Autowired
     private UserFeign userFeign;
 
-    @GetMapping("/findAll/{index}/{limit}")
-    public List<User> findAll(@PathVariable("index") int index, @PathVariable("limit") int limit){
-        return userFeign.findAll(index, limit);
+    @GetMapping("/findAll")
+    @ResponseBody
+    public UserVO findAll(@RequestParam("page") int page, @RequestParam("limit") int limit){
+        int index = (page-1)*limit;
+        UserVO userVO = new UserVO();
+        userVO.setCode(0);
+        userVO.setMsg("");
+        userVO.setCount(userFeign.count());
+        userVO.setData(userFeign.findAll(index, limit));
+        return userVO;
     }
 
-    @GetMapping("/findById/{id}")
-    public User findById(@PathVariable("id") long id){
-        return userFeign.findById(id);
+    @GetMapping("/redirect/{location}")
+    public String redirect(@PathVariable("location") String location){
+        return location;
     }
+
 
     @GetMapping("/count")
     public int count(){
@@ -30,17 +41,16 @@ public class UserHandler {
     }
 
     @PostMapping("/save")
-    public void save(@RequestBody User user){
+    public String save(User user){
+        user.setRegisterdate(new Date());
         userFeign.save(user);
+        return "redirect:/user/redirect/user_manage";
     }
 
-    @PutMapping("/update")
-    public void update(@RequestBody User user){
-        userFeign.update(user);
-    }
 
-    @DeleteMapping("/deleteById/{id}")
-    public void deleteById(@PathVariable("id") long id){
+    @GetMapping("/deleteById/{id}")
+    public String deleteById(@PathVariable("id") long id){
         userFeign.deleteById(id);
+        return "redirect:/user/redirect/user_manage";
     }
 }
